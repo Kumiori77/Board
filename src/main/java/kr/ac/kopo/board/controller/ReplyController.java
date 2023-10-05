31cec8a0e4aafbad06c045223374a8d947b1f5b2
Kumiori <dannylee88@naver.com>
2023-10-05 16:54:50 +0900

    댓글 처리 모달창 추가 댓글 등록 & 삭제 구현

--- src/main/java/kr/ac/kopo/board/controller/ReplyController.java
+++ src/main/java/kr/ac/kopo/board/controller/ReplyController.java
@@ -8,10 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +27,26 @@ public class ReplyController {
 
         return new ResponseEntity<>(replyService.getReplyList(bno), HttpStatus.OK);
 
+    }
+    
+    // 댓글 등록 처리
+    @PostMapping("")
+    public ResponseEntity<Long> register(@RequestBody ReplyDTO replyDTO){
+        log.info(replyDTO);
+        // DB의 댓글 테이블에 새로운 댓글 저장
+        Long rno = replyService.register(replyDTO);
+
+        return new ResponseEntity<>(rno, HttpStatus.OK);
+    }
+
+    // 댓글 삭제 처리
+    @DeleteMapping("/{rno}")
+    public ResponseEntity<String> remove(@PathVariable("rno") Long rno){
+        log.info("DELETE rno : " + rno);
+
+        replyService.remove(rno); // 댓글 삭제
+
+        return new ResponseEntity<>("success", HttpStatus.OK);
     }
 
 }
